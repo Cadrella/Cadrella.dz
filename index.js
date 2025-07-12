61f0e349cfcfdd7995cad3a0a4735e7c3cf80f26
Adel Manseur <ad.manseur@univ-alger.dz>
2025-07-12 01:26:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ const server = http.createServer(async (req, res) => {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(data);
             }
+            break;
 
             case '/fields': {
                 try {
